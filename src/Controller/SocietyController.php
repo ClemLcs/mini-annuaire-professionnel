2574b26c3817829ea24af58ec555d7b465b74cb1
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-09-26 11:02:40 +0200

    Ajout de la méthode readAllSociety dans le SocietyController

--- src/Controller/SocietyController.php
+++ src/Controller/SocietyController.php
@@ -4,9 +4,23 @@
 namespace App\Controller;
 
 
+use App\Repository\SocietyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SocietyController extends AbstractController
 {
+    /**
+     * Méthode permettant d'afficher toutes les sociétés
+     * @Route("/", name="readAllSociety")
+     * @param SocietyRepository $societyRepository
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function readAllSociety(SocietyRepository $societyRepository){
+        $societies = $societyRepository->findAll();
 
+        return $this->render('Society/readAll.html.twig', [
+            'societies' => $societies
+        ]);
+    }
 }
