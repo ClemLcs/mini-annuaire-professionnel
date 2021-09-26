580ee7d540961e05ae8481897d16608d954513b6
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2021-09-26 10:40:55 +0200

    Ajout de la relation ManyToOne entre la table Society et Category

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,6 +24,16 @@ class Category
      */
     private $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Society::class, mappedBy="category")
+     */
+    private $societies;
+
+    public function __construct()
+    {
+        $this->societies = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -35,6 +47,36 @@ class Category
     public function setName(?string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Society[]
+     */
+    public function getSocieties(): Collection
+    {
+        return $this->societies;
+    }
+
+    public function addSociety(Society $society): self
+    {
+        if (!$this->societies->contains($society)) {
+            $this->societies[] = $society;
+            $society->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSociety(Society $society): self
+    {
+        if ($this->societies->removeElement($society)) {
+            // set the owning side to null (unless already changed)
+            if ($society->getCategory() === $this) {
+                $society->setCategory(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Society.php
+++ src/Entity/Society.php
@@ -37,6 +37,11 @@ class Society
      */
     private $description;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="societies")
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -86,6 +91,18 @@ class Society
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
